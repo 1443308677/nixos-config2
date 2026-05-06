6640c2f58a19f29b43a23c0cf8e3c348f45d6966
1443308677 <1443308677@qq.com>
2026-05-07 01:16:19 +0800

    fix: use services.xserver.windowManager.niri.enable for niri

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -17,7 +17,7 @@
   # ------------------------------
   # Niri 窗口管理器配置
   # ------------------------------
-  services.displayManager.defaultSession = "niri";
+  services.xserver.windowManager.niri.enable = true;
 
   # ------------------------------
   # 音频系统配置
